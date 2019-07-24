aef967078709cb79833994c8cc1b10b6563c3afe
vicziani <viczian.istvan@gmail.com>
2019-07-24 14:08:52 +0200

    Verziószámok frissítése

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 ﻿JTechLog soapUI
 ===============
 
-Ez a program a JTechLog (<http://jtechlog.blogspot.com>) blog "Webszolgáltatások integrációs 
-tesztelése soapUI és JUnit használatával" cikkéhez készült példaprogram. 
-Prezentálja azt, hogyan lehet webszolgáltatásokat automatikusan, JUnit-ból, soapUI használatával
-tesztelni. Egy POJO van JAX-WS webszolgáltatásként kiajánlva, és tesztelve. Maven-nel build-elhető, 
+Ez a program a JTechLog (<http://jtechlog.hu>) blog "Webszolgáltatások integrációs 
+tesztelése SoapUI és JUnit használatával" posztjához készült példaprogram. 
+Prezentálja azt, hogyan lehet webszolgáltatásokat automatikusan, JUnitból, SoapUI használatával
+tesztelni. Egy POJO van JAX-WS webszolgáltatásként kiajánlva, és tesztelve. Mavennel buildelhető, 
 és a letöltést követően a 'mvn jetty:run' paranccsal futtatható. A webszolgáltatás a
 http://localhost:8080/services/TempConvert címen érhető el, innen van linkelve a WSDL állomány is.
 A unit tesztek az 'mvn test' parancs kiadásával futtathatóak.
 
-Felhasznált technológiák: Servlet 2.5, JAX-WS 2.2.6-2, 
-JUnit 4.10, soapUI 4.5.1, Maven 3.0.3, Jetty 8.0.4
+Felhasznált technológiák: JAX-WS, JUnit, SoapUI, Maven, Jetty
 
 viczian.istvan a gmail-en
--- src/test/java/jtechlog/soapui/test/TempConvertIntegrationTest.java
+++ src/test/java/jtechlog/soapui/test/TempConvertIntegrationTest.java
@@ -1,38 +1,35 @@
 package jtechlog.soapui.test;
 
-import java.net.MalformedURLException;
-
 import javax.xml.ws.Endpoint;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import jtechlog.soapui.TempConvert;
 
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class TempConvertIntegrationTest {
 
-    private static String address;
+    static String address;
 
-    private static Endpoint ep;
+    static Endpoint ep;
 
 
-    @BeforeClass
-    public static void beforeClass() throws MalformedURLException {
+    @BeforeAll
+    static void beforeClass() {
         address = "http://localhost:9000/TempConvert";
         ep = Endpoint.publish(address, new TempConvert());
     }
 
-    @AfterClass
-    public static void afterClass() {
+    @AfterAll
+    static void afterClass() {
         ep.stop();
     }
 
 
     @Test
-    public void testTempConvert() throws Exception {
+    void testTempConvert() throws Exception {
         SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
         runner.setProjectFile( "src/test/resources/TempConvert-soapui-project.xml" );
         runner.setEndpoint(address);
